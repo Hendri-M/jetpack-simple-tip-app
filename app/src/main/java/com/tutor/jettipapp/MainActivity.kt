bf9f5f5c2c490e670af0881d1d86d9bc4ee89487
Hendri-M <hendrimuji31@gmail.com>
2024-12-05 12:55:36 +0700

    add: input field on main content

--- app/src/main/java/com/tutor/jettipapp/MainActivity.kt
+++ app/src/main/java/com/tutor/jettipapp/MainActivity.kt
@@ -12,21 +12,27 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.tutor.jettipapp.components.InputField
 import com.tutor.jettipapp.ui.theme.JetTipAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,7 +40,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-                TopHeader()
+//                TopHeader()
+                MainContent()
             }
         }
     }
@@ -60,7 +67,7 @@ fun TopHeader (totalAmount: Double = 254.0) {
             .fillMaxWidth()
             .height(175.dp)
             .clip(shape = RoundedCornerShape(12.dp)),
-        color = Color(0x59A86FDF)
+        color = Color(0xCBC28AF8)
     ) {
         Column(
             modifier = Modifier
@@ -68,10 +75,37 @@ fun TopHeader (totalAmount: Double = 254.0) {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-//            TODO:  Format Value
+            /* TODO:  Format Value */
             val value = "%.2f".format(totalAmount)
+            /* END OF TODO */
             Text(text = "Total Per Person", style = MaterialTheme.typography.headlineSmall, fontWeight = FontWeight.Bold)
             Text(text = "$$value", style = MaterialTheme.typography.displaySmall, fontWeight = FontWeight.ExtraBold)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun MainContent() {
+    val totalBillState = remember {
+        mutableStateOf("")
+    }
+    val validState = remember(totalBillState.value) {
+        totalBillState.value.trim().isNotEmpty()
+    }
+    val keyboardController = LocalSoftwareKeyboardController.current
+    Surface(modifier = Modifier
+        .padding(4.dp)
+        .fillMaxWidth(),
+        shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+        border = BorderStroke(2.dp, Color.LightGray)
+    ) {
+        Column(modifier = Modifier.padding(4.dp)) {
+            InputField(valueState = totalBillState, labelId = "Enter Bill", enabled = true, isSingleLine = true, onAction = KeyboardActions{
+                if(!validState) return@KeyboardActions
+                // TODO - OnValueChange
+                keyboardController?.hide()
+            } )
         }
     }
 }
